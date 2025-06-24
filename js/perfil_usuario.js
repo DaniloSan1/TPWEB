86af53d15c631d0ac6e05a26f2f2f0dd3dc9060f
CrisColombo <cristiangcolombo@hotmail.com>
2025-06-23 22:33:10 -0300

    cambios perfil usuario(sin terminar)

--- js/perfil_usuario.js
+++ js/perfil_usuario.js
@@ -1,3 +1,49 @@
+const ERROR_MESSAGES = {
+  CONTRA_INVALIDA:
+    "La contraseña debe tener minimo 2 letras, 2 numeros y 2 caracteres especiales.",
+  REPETIR_CONTRA_INVALIDA: "Las contraseñas deben ser iguales.",
+};
+
+//Validar Contraseña
+  const nuevaContra = document.querySelector("#nueva_contrasena").value.trim();
+  const repeContra = document.querySelector("#repetir_contrasena").value.trim();
+  const errorContrasena = document.querySelector(".js-contrasena-error");
+  const errorRepetirContra = document.querySelector(
+      ".js-repetircontra-error"
+    );
+
+      errorContrasena.textContent = "";
+      errorRepetirContra.textContent = "";
+
+function validarContrasena(nuevaContra) {
+      if (nuevaContra.length < 8) return false;
+
+      let letras = 0;
+      let numeros = 0;
+      let especiales = 0;
+
+      for (let i = 0; i < nuevaContra.length; i++) {
+        const char = nuevaContra[i];
+        if (/[a-zA-Z]/.test(char)) {
+          letras++;
+        } else if (/[0-9]/.test(char)) {
+          numeros++;
+        } else if (/[^a-zA-Z0-9]/.test(char)) {
+          especiales++;
+        }
+      }
+      return letras >= 2 && numeros >= 2 && especiales >= 2;
+    }
+
+    if (repeContra !== contra) {
+      errorRepetirContra.textContent = ERROR_MESSAGES.REPETIR_CONTRA_INVALIDA;
+      isFormValid = false;
+    };
+    
+    validarContrasena(nuevaContra);
+
+
+
 document.addEventListener("DOMContentLoaded", function () {
   const usuarioActivo = JSON.parse(localStorage.getItem("usuarioActivo"));
   let usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
